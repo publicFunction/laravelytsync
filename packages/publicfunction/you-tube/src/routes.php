0e95156ae77bbbc41ec8c3dbd33cb6268e4f8080
Chris Moss <Chris Moss>
2016-02-17 23:39:45 +0000

    removed packages in prep for submodules

--- packages/publicfunction/you-tube/src/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::get('you-tube/', '\PublicFunction\YouTube\YouTubeController@index');
-Route::get('you-tube/playlists', '\PublicFunction\YouTube\YouTubeController@playlists');
-Route::get('you-tube/videos', '\PublicFunction\YouTube\YouTubeController@videos');
